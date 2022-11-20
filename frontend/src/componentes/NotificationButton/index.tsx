ccacf6c690b0930b268fedc53e023e9cdcf10f86
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-20 14:14:57 -0300

    Axios, useEffect first request

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
 import icon from '../../assets/img/notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NoticationButton() {
     return (
@@ -10,4 +10,4 @@ function NoticationButton() {
     )
 }
 
-export default NoticationButton
+export default NoticationButton;
